5bc06cd356722c49f6ed38a87aa0dbd64d460604
Ha Duy Duong <haduyduong06031902@gmail.com>
2024-11-14 23:59:05 +0700

    Done Lab 6.1

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -5,6 +8,10 @@ public class Main {
         System.out.println("Hello, OOP");
         System.out.println("----- Lab 6.1 -----");
         System.out.println("Regular Expression");
-        int a =0;
+        System.out.println("-------------------");
+
+        Pattern pattern = Pattern.compile("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$");
+        Matcher matcher = pattern.matcher("haduyduong06031902@gmail.com");
+        System.out.println("Input String matches regex - "+matcher.matches());
     }
 }
